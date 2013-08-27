01669eeb9b21be67b287c746d2b9e59a90ae15b3
Patrick Jaromin <patrick@jaromin.com>
2013-08-27 17:49:18 -0500

    Added apache license banner to source files

--- hbase-matchers/src/main/java/com/jaromin/hbase/matchers/ColumnMatcher.java
+++ hbase-matchers/src/main/java/com/jaromin/hbase/matchers/ColumnMatcher.java
@@ -1,5 +1,21 @@
 package com.jaromin.hbase.matchers;
 
+/**
+ * Copyright 2013 Patrick K. Jaromin <patrick@jaromin.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -23,6 +39,7 @@ public class ColumnMatcher<T> extends TypeSafeDiagnosingMatcher<Mutation> {
 
 	private Matcher<? super T> nameMatcher;
 	
+	@SuppressWarnings("unused") // still hoping to utilized; can't bring myself to remove yet ;)
 	private Class<T> nameTypeClass;
 	
 	/**
